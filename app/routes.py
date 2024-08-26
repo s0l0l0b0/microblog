45ceb7a16e04e238e9275f065614c0eada3d5987
s0l0l0b0 <smshaon11@gmail.com>
2024-08-26 16:23:17 +0800

    creating base template is done and working as it should be

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask  import render_template
 from app import app
 
 # Decorators: modifies the fucntion that follows it.
@@ -7,4 +8,15 @@ from app import app
 @app.route('/index')
 
 def index():
-    return "Hello, World!, anything else?"
+    user = {'username': 'father'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
