3384b3eb5989d8d3396d21d9822613238647dbe4
s0l0l0b0 <smshaon11@gmail.com>
2024-09-01 17:06:45 +0800

    Created two new files forms.py and login.html; created a route for login in routes.py; added the login page to the base/home page.

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from flask  import render_template
 from app import app
+from app.forms import LoginForm
 
 # Decorators: modifies the fucntion that follows it.
 # A common pattern with decorators is to use them to register functions as callbacks for certain events.
 # In this case, the @app.route decorator creates an association between the URL given as an argument and the function.
 @app.route('/')
-@app.route('/index')
 
+
+@app.route('/index')
 def index():
     user = {'username': 'father'}
     posts = [
@@ -20,3 +22,10 @@ def index():
         }
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+# This function mapped to the /login URL that creates a form
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
