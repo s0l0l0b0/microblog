3053611348c2cd144a18d0b7afc42b033ccfe2cc
s0l0l0b0 <smshaon11@gmail.com>
2024-09-24 00:13:19 +0800

    successfully completed the user login

--- app/models.py
+++ app/models.py
@@ -3,9 +3,14 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+from flask_login import UserMixin # for Flask-Loing
+from app import login # for User Loader Function
+
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -15,6 +20,12 @@ class User(db.Model):
     # realtionship between users and posts
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
     # tells Python how to print objects of this class; useful for debugging
@@ -33,3 +44,10 @@ class Post(db.Model):
 
     def __repr__(self) -> str:
         return '<Post {}>'.format(self.body)
+    
+
+# to keep track of the logged in user
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,27 @@ from flask  import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+from flask_login import logout_user
+
+from flask_login import login_required
+
+from flask import request
+from urllib.parse import urlsplit
+
 # Decorators: modifies the fucntion that follows it.
 # A common pattern with decorators is to use them to register functions as callbacks for certain events.
 # In this case, the @app.route decorator creates an association between the URL given as an argument and the function.
 @app.route('/')
-
-
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'father'}
+    # user = {'username': 'father'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,14 +33,31 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 # This function mapped to the /login URL that creates a form
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password.')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next') 
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
